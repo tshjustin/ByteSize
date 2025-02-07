396ec5a88327c4e95eb0021bd290269202422dbf
tshjustin <j.tann2305@gmail.com>
2025-02-07 23:11:21 +0800

    Added Model Class

--- bytesize-backend/bytesize/models/paper.py
+++ bytesize-backend/bytesize/models/paper.py
@@ -1,3 +1,17 @@
+"""Data model class for POSTGRES"""
 
-@dataclass
-### 
+from sqlalchemy import Column, Integer, String, ARRAY, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Paper(Base):
+    __tablename__ = "papers"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    authors = Column(ARRAY(String))
+    published = Column(DateTime)
+    summary = Column(String)
+    link = Column(String)
+    categories = Column(ARRAY(String))
