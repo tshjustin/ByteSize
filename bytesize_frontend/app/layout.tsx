7d807f919a5ed3aa508f24f0529e1123245ec886
tshjustin <j.tann2305@gmail.com>
2025-02-28 11:46:12 +0800

    Fixed import errors

--- bytesize_frontend/app/layout.tsx
+++ bytesize_frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
-import { Navigation } from '@/components/navigation';
+import { Navigation } from '@/components/Navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
